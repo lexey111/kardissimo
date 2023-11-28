89d2ff29cbc272d564b93d9235eff840e89e0274
Oleksii Koshkin <lexey111@gmail.com>
2023-11-28 18:51:28 +0100

    Footer tuning

--- src/components/app-footer.component.tsx
+++ src/components/app-footer.component.tsx
@@ -4,6 +4,7 @@ export const AppFooter: React.FC = () => {
 	return <div id='app-footer'>
 		<div id='app-footer-content'>
 			&copy; 2023 Oleksii Koshkin aka lexey111
+			<span>Version 1.0.0</span>
 		</div>
 	</div>;
 };
