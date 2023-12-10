21f8860e1cf7f2452950866e623b74e5b6d097e4
Oleksii Koshkin <lexey111@gmail.com>
2023-12-10 12:01:32 +0100

    Hidden faq

--- src/components/app-footer.component.tsx
+++ src/components/app-footer.component.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useCallback} from "react";
+import {useNavigate} from "react-router-dom";
 
 export const AppFooter: React.FC = () => {
+	const navigate = useNavigate();
+
+	const goFaq = useCallback(() => {
+		navigate('/faq');
+	}, []);
+
 	return <div id='app-footer'>
 		<div id='app-footer-content'>
 			&copy; 2023 Oleksii Koshkin aka lexey111
-			<span>Version 1.0.0</span>
+			<span onClick={goFaq}>Version 1.0.0</span>
 		</div>
 	</div>;
 };
